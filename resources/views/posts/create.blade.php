e8c5356d1859b05bbc5ed12afb4a168deea5701e
akiiii04 <hiroaki200204@gmail.com>
2023-06-18 17:31:03 +0900

    08-4-2

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,12 +9,13 @@
         <h1 class="webtitle">ブログ投稿フォーム</h1>
         <form action="/posts" method="POST">
             @csrf
-
         <div class='posts'>
             <h2 class='title'>タイトル</h2>
-                <input type="text" name="post[title]" >
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title')}}"/>
+                <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
             <h2 class='body'>本文</h2>
-                <textarea name="post[body]" ></textarea>
+                <textarea name="post[body]" >{{ old('post.body')}}</textarea>
+                 <p class="body_error" style="color:red">{{ $errors->first('post.body') }}</p>
         </div>
         <input type="submit" value="保存"/>
 
